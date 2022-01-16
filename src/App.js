25d61d4e96569362fb79ad97c052e2d69f670c49
addincendekia <addin.cendekia@sirclo.com>
2022-01-16 09:34:38 +0000

    refactor(project - structure): setup structure folder and add tailwind package

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="text-center">
+      <header className="bg-gray-900 flex flex-col items-center justify-center min-h-screen text-2xl text-white">
+        <img src={logo} className="h-96 w-96" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="text-ocean"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
